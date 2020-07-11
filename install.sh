23460323a387f70035f8199fa59ddeef5753f273
Capybara Translation <capybara.translation@gmail.com>
2020-07-11 13:25:09 +0900

    add .ideavimrc

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ mkdir -p ${HOME}/.config/nvim
 cd `dirname $0`
 ln -s ${PWD}/init.vim ${HOME}/.config/nvim/init.vim
 ln -s ${PWD}/commands.vim ${HOME}/.config/nvim/commands.vim
+ln -s ${PWD}/.ideavimrc ${HOME}/.ideavimrc
